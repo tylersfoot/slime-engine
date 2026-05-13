a0fd7aec917f9ac314ee10d98c536df01123cd54
tylersfoot <tylerdepa8@gmail.com>
2026-05-13 18:33:12 -0400

    Change build script to fully reset output directory on build

--- build.rs
+++ build.rs
@@ -1,19 +1,20 @@
 // this build script gets run before the rest of the code is compiled
 // it copies the `res/` into the output directory 
-use anyhow::*;
-use fs_extra::copy_items;
-use fs_extra::dir::CopyOptions;
-use std::env;
-
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     // this tells Cargo to rerun this script if something in /res/ changes
     println!("cargo:rerun-if-changed=res");
 
-    let out_dir = env::var("OUT_DIR")?;
-    let mut copy_options = CopyOptions::new();
+    let out_dir = std::env::var("OUT_DIR")?;
+    let mut copy_options = fs_extra::dir::CopyOptions::new();
     copy_options.overwrite = true;
     let paths_to_copy = vec!["res/"];
-    copy_items(&paths_to_copy, out_dir, &copy_options)?;
+
+    // wipe folder to fully sync files
+    let dest_path = std::path::Path::new(&out_dir).join("res");
+    if dest_path.exists() {
+        std::fs::remove_dir_all(&dest_path)?;
+    }
+    fs_extra::copy_items(&paths_to_copy, out_dir, &copy_options)?;
 
     Ok(())
 }
